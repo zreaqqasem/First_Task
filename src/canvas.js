eb943a0b6a8ea01d2ef1a8b547d5ae99f2d2e255
qasem <zreaqqasem@ymail.com>
2020-01-10 22:42:20 +0200

    branch-four-done

--- src/canvas.js
+++ src/canvas.js
@@ -5,8 +5,8 @@ export default class canvas {
     this.my_canvas = document.getElementById("firstTask");
     this.context = this.my_canvas.getContext("2d");
     this.ballImg = new Image();
-    this.ballImg.src = "../First_Task/images/circle-cropped.png";
+    this.ballImg.src = "../images/circle-cropped.png";
     this.holeImg = new Image();
-    this.holeImg.src = "../First_Task/images/hole.png";
+    this.holeImg.src = "../images/hole.png";
   }
 }
